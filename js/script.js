61e4cf5c6ef34debc263611eea1dd200bdbd02d2
gkumarihawk <74431929+gkumarihawk@users.noreply.github.com>
2023-04-04 00:40:38 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 function init(){
 //add your javascrip between these two lines of code
 	var entryinput = document.getElementById('entryinput');
+	var userInput = entryinput.value;
  	function myEventFunction (){
- 		alert ('Gauri Kumari:' + entryinput);
+ 		alert ('Gauri Kumari:' + userInput);
  	}
 
  	var entrybutton = document.getElementById('entrybutton');
